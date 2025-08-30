e27215b0de34f5dddf8890cc8624bba6def41331
Tanusdech <6749010010@phuketvc.ac.th>
2025-08-30 16:04:40 +0700

    Step 3: Check internet connection, Step 4: Show toast message

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'dart:async';
 
+// Step 3: Check internet connection
+import 'package:connectivity_plus/connectivity_plus.dart';
+// Step 4: Show toast message
+import 'package:fluttertoast/fluttertoast.dart';
+
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
 
@@ -13,13 +18,51 @@ class _FirstScreenState extends State<FirstScreen> {
   void initState() {
     super.initState();
 
-    Timer(
-      const Duration(seconds: 3),
-      () => Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const SecondScreen()),
-      ),
-    );
+    // Timer(
+    //   const Duration(seconds: 3),
+    //   () => Navigator.push(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => const SecondScreen()),
+    //   ),
+    // );
+
+    // Step 3: Check internet connection
+    checkInternetConnection();
+  }
+  @override
+  // Step 3: Check internet connection
+  void checkInternetConnection() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult.contains(ConnectivityResult.mobile)) {
+      // Mobile network available.
+      _showToast(context, "Mobile network available.");
+    } else if (connectivityResult.contains(ConnectivityResult.wifi)) {
+      // Wi-fi is available.
+      // Note for Android:
+      // When both mobile and Wi-Fi are turned on system will return Wi-Fi only as active network type
+      _showToast(context, "Wi-fi is available.");
+    } else if (connectivityResult.contains(ConnectivityResult.ethernet)) {
+      // Ethernet connection available.
+      _showToast(context, "Ethernet connection available.");
+    } else if (connectivityResult.contains(ConnectivityResult.vpn)) {
+      // Vpn connection active.
+      // Note for iOS and macOS:
+      // There is no separate network interface type for [vpn].
+      // It returns [other] on any device (also simulator)
+      _showToast(context, "Vpn connection active.");
+    } else if (connectivityResult.contains(ConnectivityResult.bluetooth)) {
+      // Bluetooth connection available.
+      _showToast(context, "Bluetooth connection available.");
+    } else if (connectivityResult.contains(ConnectivityResult.other)) {
+      // Connected to a network which is not in the above mentioned networks.
+      _showToast(context, "Other network is available.");
+    } else if (connectivityResult.contains(ConnectivityResult.none)) {
+      // No available network types
+      setState(() {
+        showAlertDialog(context, "No internet connection",
+            "Please check your internet connection and try again.");
+      });
+    }
   }
 
   Widget build(BuildContext context) {
@@ -50,6 +93,71 @@ class _FirstScreenState extends State<FirstScreen> {
   }
 }
 
+// Step 4: Show toast message
+void _timer(BuildContext context) {
+  Timer(
+    const Duration(seconds: 3),
+    () => Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SecondScreen()),
+    ),
+  );
+}
+
+// Step 4: Show toast message
+void _showToast(BuildContext context, String msg) {
+  Fluttertoast.showToast(
+    msg: msg,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+    timeInSecForIosWeb: 1,
+    backgroundColor: Colors.lightGreen,
+    textColor: Colors.white,
+    fontSize: 24.0,
+  );
+  _timer(context);
+}
+
+// Step 4: Show toast message
+void showAlertDialog(BuildContext context, String title, String msg) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 24,
+            color: Colors.redAccent,
+            fontWeight: FontWeight.w500,
+            fontFamily: "Alike",
+          ),
+        ),
+        content: Text(msg),
+        actions: <Widget>[
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.black54),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              "OK",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.blue.shade200,
+                fontWeight: FontWeight.w500,
+                fontFamily: "Alike",
+              ),
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
+
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
 
@@ -61,7 +169,7 @@ class SecondScreen extends StatelessWidget {
           'This is the second screen',
           style: TextStyle(
             fontSize: 24,
-            color: Colors.greenAccent,
+            color: Colors.orangeAccent,
             fontWeight: FontWeight.w500,
             fontFamily: "Alike",
           ),
